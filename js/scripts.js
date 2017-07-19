a1013964faf4d0b9a8e4bf1dc03b2576bb8271cb
Esti Shay and Kate Mashokha <kate@>
2017-07-19 13:33:16 -0700

    allow player to make Space constructor as played

--- js/scripts.js
+++ js/scripts.js
@@ -16,27 +16,40 @@ Board.prototype.spaceCreation = function() {
   }
 };
 
-Board.prototype.isWon = function(player) {
-  spaceArray.forEach...
-  if () {
-
-    }
-  }
-};
+// Board.prototype.isWon = function(player) {
+//   spaceArray.forEach...
+//   if () {
+//
+//     }
+//   }
+// };
 
 function Space(x,y,cell) {
   this.xCoordinate = x;
   this.yCoordinate = y;
   this.cell = cell;
-  this.player = 'X';
+  this.player = '';
 }
 
 
 
 //UI logic
 $(document).ready(function () {
+
+  var ourBoard = new Board();
+  var boardArray = ourBoard.spaceCreation();
+
   $(".box").click(function() {
+
     $(this).text("X");
-    alert(this.id);
+
+    for (var i = 0; i < ourBoard.spaceArray.length; i++) {
+      if (ourBoard.spaceArray[i].cell === this.id) {
+        ourBoard.spaceArray[i].player = 'X';
+      }
+    }
+    console.log(ourBoard.spaceArray);
+
+    // alert(this.id);
   })
 });
