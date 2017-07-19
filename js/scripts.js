3496b10941f9afc81f12bc2074bc9736df068b06
Esti Shay and Kate Mashokha <kate@>
2017-07-19 11:24:40 -0700

    create Board constructor which creates Space objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
+//Business logic
+function Player(mark) {
+  this.mark = mark;
+}
+
+function Board() {
+  this.spaceArray = [];
+}
+
+Board.prototype.spaceCreation = function() {
+  for (var i = 0; i < 3 ; i++) {
+    for (var j = 0; j < 3; j++) {
+      this.spaceArray.push(new Space(i,j));
+    }
+  }
+};
+
+function Space(x,y) {
+  this.xCoordinate = x;
+  this.yCoordinate = y;
+}
+
+
+
+//UI logic
 $(document).ready(function () {
   $(".box").click(function() {
     $(this).text("X");
